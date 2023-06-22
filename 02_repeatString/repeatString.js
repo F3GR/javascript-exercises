4286f4155e740c06efbee8f540b82f732197bf6a
F3GR <egmver.2@gmail.com>
2023-06-22 18:15:55 +0400

    js exercises 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let resultString = "";
 
+    if (number < 0) {
+       return resultString = "ERROR";
+    }
+
+    let i = 0;
+    
+    for (; i < number; i++) {
+        resultString += string;
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line
